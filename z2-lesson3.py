a18daae1f5119d8f94f9fb9523c033f9c1c3db98
amamchyts <anton.mamchyts@gmail.com>
2020-07-06 13:04:04 +0300

    Задание 2

---
+++ z2-lesson3.py
@@ -0,0 +1,3 @@
+def my_func(name, surname, year, city, email, phone):
+    print('Вас зовут ', name, surname,'. Вы родились в ', year,' году в городе', city,'. Ваши контактные данные', email, phone)
+my_func(name='Антон', surname='Мамчиц', year='1998', city='Москва', email='mamchitsao@mai.ru', phone='+7(777)777-77-77')
